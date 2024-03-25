a287f4169d0c9aab1a2a5e6cb6ff4b3cf4c265e4
José Pedro Sousa <outgoing@zpedro.dev>
2024-03-25 17:35:51 +0000

    taking shape, damn im so excited lol

--- epoch_conv.js
+++ epoch_conv.js
@@ -2,6 +2,3 @@ function toEpoch(time) {
 	let [hours, minutes] = time.split(".").map(Number);
 	return (hours * 60 + minutes) / 10;
 }
-
-// example for Peasant
-// ["10.00", "13.00", "16.00", "19.00", "22.00"].map(toEpoch);
